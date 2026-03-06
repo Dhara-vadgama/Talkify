88d49abe958bfc86ea2b84fb1a77a7665795d9d7
Dhara Vadgama <dharavadgama11@gmail.com>
2026-03-06 10:31:48 +0530

    environment.js added

--- frontend/environment.js
+++ frontend/environment.js
@@ -1,5 +1,6 @@
-const server = {
-    dev:"http://localhost:8000",
-    prod:"https://talkify-32sl.onrender.com"
-}
+let Is_PROD = true;
+
+const server = Is_PROD ?"https://talkify-32sl.onrender.com" : "http://localhost:8000";
+    
+
 export default server;
