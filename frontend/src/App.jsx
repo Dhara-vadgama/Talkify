459a3fccd888232668c7c9bd3f52285fcd8c640b
Dhara Vadgama <dharavadgama11@gmail.com>
2026-04-08 09:14:29 +0530

    changes applied

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,18 +6,22 @@ import { AuthProvider } from "./context/authContext";
 import VideoMeet from "./pages/videoMeet";
 import History from "./pages/History";
 import HomeComponent from "./pages/home";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <>
       <Router>
         <AuthProvider>
+        <Toaster />
+       
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/auth" element={<Authentication />} />
-            <Route path='/home's element={<HomeComponent />} />
-            <Route path="/:url" element={<VideoMeet />} />
+            <Route path='/home' element={<HomeComponent />} />
+           
             <Route path="/history" element={<History />} />
+            <Route path="/:url" element={<VideoMeet />} />
           </Routes>
         </AuthProvider>
       </Router>
